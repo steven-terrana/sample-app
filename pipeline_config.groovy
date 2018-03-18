b908ced409e9dedbe8e2b70e3a8d307d124475f8
steven-terrana <steventerrana@gmail.com>
2018-03-18 12:12:16 -0400

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,25 +1,3 @@
-organization = "organization"
-
-use_pipeline_template{
-    
-}
-
-application_environments{
-    dev{
-        bananas = 6
-        short_name = "tenant dev"
-    }
-    test{
-        short_name = "tenant_test"
-        long_name = "Tenant Test"
-    }
-    staging{
-        short_name = "Tenant Staging"
-        long_name = "Tenant Staging"
-        bananas = 12
-    }
-}
-
 libraries{
     docker{ 
         branch = "testing" 
